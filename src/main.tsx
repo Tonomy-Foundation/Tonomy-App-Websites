ec4553cd1e73e2a4d21085030ac39bd74aae096c
rebal67 <rebalree1@gmail.com>
2023-03-08 12:10:57 +0100

    fix vite analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-const demoModule = "./demo";
 
 /**
  * using dynamic import to have less code depending on the subdomain
  *
  * this solution can also be done using multiple servers in vite to load root based on subdomain
  */
-console.log(window.origin);
+
+console.log(window.location.port);
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 console.log(window.location.href);
 try {
-  import(demoModule).then((module) => {
-    const demo = module.default;
-    demo(root);
-  });
+  if(parseInt(window.location.port) === 3001){
+    import("./demo/module-index.js").then((module) => {
+      const demo = module.default;
+      demo(root);
+    });
+  }else if(parseInt(window.location.port) === 3000){
+    import("./demo/module-index.js").then((module) => {
+      const demo = module.default;
+      demo(root);
+    });
+  }
+  
 } catch (e) {
   console.log(e);
 }
