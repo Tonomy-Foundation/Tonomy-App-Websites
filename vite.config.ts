4b9533de04d497bfc91365d10065fe2cd0ccc893
rebal67 <rebalree1@gmail.com>
2023-03-08 11:57:34 +0100

    run on multiple ports

--- vite.config.ts
+++ vite.config.ts
@@ -7,34 +7,9 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
   const config: UserConfigExport = {
     plugins: [react()],
     server: {
-      proxy: {
-        "/": {
-          target: "http://localhost:5173",
-          changeOrigin: false,
-          configure: (proxy, _options) => {
-            proxy.on("error", (err, _req, _res) => {
-              console.log("proxy error", err);
-            });
-            proxy.on("proxyReq", (proxyReq, req, _res) => {
-              // Get the subdomain from the request hostname
-              const subdomain = req.headers.host?.split(".")[0];
-              // proxyReq.setHeader(
-              //   "referer",
-              //   `http://${subdomain}.localhost:1756`
-              // );
-              // proxyReq.setHeader(
-              //   "origin",
-              //   `http://${subdomain}.localhost:1756`
-              // );
-              proxyReq.path = `/${subdomain}${req.url}`;
-            });
-          },
-          rewrite: (path) => path.replace(/^\/\w+/, ""),
-        },
-        
-      },
-    
-    },
+      port: 5174,
+      strictPort: true
+    }
   };
 
   if (process.env.VITE_APP_NODE_ENV == "local") {
