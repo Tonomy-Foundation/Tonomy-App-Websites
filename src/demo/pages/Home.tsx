e87882224ab166e3e1fda0fadca401cbf8f1916f
StefanoPitto <stefanopitto1@gmail.com>
2023-04-27 17:21:36 -0300

    fix: :sparkles: add new component
    now the logged in screen is in a new component and does not replace the other home screen, also added navigation con "/logged-in" route

--- src/demo/pages/Home.tsx
+++ src/demo/pages/Home.tsx
@@ -2,8 +2,10 @@ import React, { useEffect } from "react";
 import { api, SdkError, SdkErrors } from "@tonomy/tonomy-id-sdk";
 import settings from "../settings";
 import { api, SdkError, SdkErrors, ExternalUser } from "@tonomy/tonomy-id-sdk";
+import settings from "../settings";
 import "./Home.css";
 import { TH1, TH3, TP } from "../../sso/components/THeadings";
+import logo from "../assets/tonomy/tonomy-logo48.png";
 import { Highlighter } from "rc-highlight";
 import "@tonomy/tonomy-id-sdk/build/api/tonomy.css";
 
@@ -37,38 +39,22 @@ export default function Home() {
   return (
     <div className="container">
       <div className="intro">
-        <TP className="head-subtitle">You are now logged in with Tonomy ID.</TP>
-        <TH3 className="text-title">Home</TH3>
-        <TP className="text-header">
-          Our demo site showcases the benefits of Tonomy ID for both users and
-          administrators. <br />
-          As a user, you now have access to a variety of features.
+        <header>
+          <img src={logo} alt="" />
+        </header>
+        <TH1>Tonomy ID DEMO</TH1>
+        <TP size={2} className="text-header">
+          Welcome to our Tonomy ID Demo website!
         </TP>
-        <div className="example-container">
-          <TP>
-            You can create a verifiable data by using our built-in tools to
-            create and manage your own digital certificates
-          </TP>
-          <button className="tonomy-login-button">
-            SIGN W3C VERIFIABLE CREDENTIALS
-          </button>
-        </div>
-        <div className="example-container">
-          <TP>
-            You can sign blockchain transactions using our secure system and
-            your private key.
-          </TP>
-          <button className="tonomy-login-button">
-            SIGN BLOCKCHAIN TRANSACTIONS
-          </button>
-        </div>
-        <div className="example-container">
-          <TP>
-            Additionally, you can send messages to other users of Tonomy ID,
-            allowing for easy communication and collaboration.
-          </TP>
-          <button className="tonomy-login-button">
-            SEND PEER TO PEER MESSAGES
+        <TP size={2} className="text-body">
+          With Tonomy ID, you can simplify login, improve security, and enhance
+          user experience by logging in to multiple applications with just one
+          set of credentials. Our demo site showcases the benefits of Tonomy ID
+          for both users and administrators.
+        </TP>
+        <div className="footer">
+          <button className="tonomy-login-button" onClick={onButtonPress}>
+            Login with {settings.config.appName}
           </button>
         </div>
       </div>
