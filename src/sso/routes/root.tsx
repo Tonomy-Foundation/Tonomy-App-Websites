14993fb13e770b5d3a2a9e778b419de7aa7f9f86
Jack Tanner <jack@jackandtheblockstalk.com>
2023-05-10 17:22:23 +0200

    fix: fixed router and removed AppDetails.tsx page

--- src/sso/routes/root.tsx
+++ src/sso/routes/root.tsx
@@ -3,18 +3,6 @@ import Home from "../pages/Home";
 import CallBackPage from "../pages/CallBack";
 import DownloadApp from "../pages/DownloadApp";
 import Login from "../pages/Login";
-import Loading from "../pages/Loading";
-import AppDetails from "../pages/AppDetails";
-
-/* Navigation flow:
-  /login
-  --> /loading (logged in)
-    --> external website (on logout / cancel)
-    --> /appDetails (app requests sent)
-      --> external website (on logout) **
-  --> /callback (after QR code scanned and the request response received)
-    --> ** external website (with the response)
-*/
 
 const router = createBrowserRouter([
   {
@@ -29,14 +17,6 @@ const router = createBrowserRouter([
     path: "/download",
     element: <DownloadApp />,
   },
-  // {
-  //   path: "/appDetails",
-  //   element: <AppDetails />,
-  // },
-  // {
-  //   path: "/loading",
-  //   element: <Loading />,
-  // },
   {
     path: "/callback",
     element: <CallBackPage />,
