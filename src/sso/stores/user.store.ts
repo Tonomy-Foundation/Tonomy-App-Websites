46583cbd5acc4f3d588d89bb3f314e1a898ceb62
rebal67 <rebalalh@gmail.com>
2023-05-12 16:22:03 +0200

    fix: move settings to store

--- src/sso/stores/user.store.ts
+++ src/sso/stores/user.store.ts
@@ -1,23 +1,29 @@
-import { Communication, ExternalUser } from "@tonomy/tonomy-id-sdk";
+import { Communication, ExternalUser, api } from "@tonomy/tonomy-id-sdk";
 import { create } from "zustand";
+import settings from "../../common/settings";
+
+api.setSettings({
+  blockchainUrl: settings.config.blockchainUrl,
+  communicationUrl: settings.config.communicationUrl,
+});
 
 interface ExternalUserStore {
-    user: ExternalUser | undefined;
-    communication: Communication;
-    isLoggedIn: () => boolean;
-    setUser: (user: ExternalUser) => void;
-    logout: () => void;
+  user: ExternalUser | undefined;
+  communication: Communication;
+  isLoggedIn: () => boolean;
+  setUser: (user: ExternalUser) => void;
+  logout: () => void;
 }
 
 export const useUserStore = create<ExternalUserStore>((set, get) => ({
-    user: undefined,
-    communication: new Communication(),
-    isLoggedIn: () => get().user !== undefined,
-    setUser: (user: ExternalUser) => {
-        set({ user });
-    },
-    logout: async () => {
-        await get().user?.logout();
-        set({ user: undefined });
-    },
+  user: undefined,
+  communication: new Communication(),
+  isLoggedIn: () => get().user !== undefined,
+  setUser: (user: ExternalUser) => {
+    set({ user });
+  },
+  logout: async () => {
+    await get().user?.logout();
+    set({ user: undefined });
+  },
 }));
