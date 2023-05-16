23bc4553f3d225c3af63315a80829902c77cd567
Jack Tanner <jack@jackandtheblockstalk.com>
2023-05-16 17:36:44 +0200

    feat: handle user cancel or login with modal

--- src/common/molecules/TModal.tsx
+++ src/common/molecules/TModal.tsx
@@ -6,7 +6,16 @@ import TIcon from "../atoms/TIcon";
 export type ModalProps = React.ComponentProps<typeof Modal> & {
   onPress: () => void;
   icon: string;
-  iconColor?: "inherit" | "action" | "disabled" | "primary" | "secondary" | "error" | "info" | "success" | "warning";
+  iconColor?:
+  | "inherit"
+  | "action"
+  | "disabled"
+  | "primary"
+  | "secondary"
+  | "error"
+  | "info"
+  | "success"
+  | "warning";
   title: string;
   buttonLabel?: string;
   open: boolean;
@@ -24,6 +33,7 @@ const styles = {
     backgroundColor: "rgba(0,0,0,0.5)",
   },
   modalContent: {
+    maxWidth: 400,
     backgroundColor: "white",
     borderRadius: 1,
     padding: 3,
@@ -32,25 +42,24 @@ const styles = {
     fontSize: 16,
     fontWeight: "bold",
     marginBottom: 16,
-  }
+  },
 };
 
 export default function TModal(props: ModalProps) {
   return (
-    <Modal
-      open={props.open}
-      onClose={props.onPress}
-    >
+    <Modal open={props.open} onClose={props.onPress}>
       <Box sx={styles.modal}>
         <Box sx={styles.modalContent}>
           <div style={{ textAlign: "center" }}>
             <TIcon color={props.iconColor} icon={props.icon} />
             <div style={styles.title}>{props.title}</div>
             {props.children}
-            <div style={{
-              marginTop: 16,
-              textAlign: "right",
-            }}>
+            <div
+              style={{
+                marginTop: 16,
+                textAlign: "right",
+              }}
+            >
               <TButton onClick={props.onPress}>
                 {props.buttonLabel ?? "OK"}
               </TButton>
@@ -60,5 +69,4 @@ export default function TModal(props: ModalProps) {
       </Box>
     </Modal>
   );
-
 }
--- src/demo/pages/Callback.tsx
+++ src/demo/pages/Callback.tsx
@@ -1,10 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { api, SdkError, SdkErrors } from "@tonomy/tonomy-id-sdk";
-import "./callback.css";
+import "./Callback.css";
 import { useNavigate } from "react-router-dom";
+import useErrorStore from "../../common/stores/errorStore";
+import TProgressCircle from "../../common/atoms/TProgressCircle";
+import { TH2 } from "../../common/atoms/THeadings";
+import TModal from "../../common/molecules/TModal";
 
 export default function Callback() {
+  const [errorTitle, setErrorTitle] = useState("");
+  const [errorVisible, setErrorVisible] = useState(false);
   const navigation = useNavigate();
+  const errorStore = useErrorStore();
 
   useEffect(() => {
     verifyLogin();
@@ -19,14 +26,45 @@ export default function Callback() {
         e instanceof SdkError &&
         (e.code === SdkErrors.UserCancelled || SdkErrors.UserLogout)
       ) {
-        alert("User cancelled login");
-        navigation("/");
+        switch (e.code) {
+          case SdkErrors.UserCancelled:
+            setErrorTitle("User cancelled login");
+            setErrorVisible(true);
+            break;
+          case SdkErrors.UserLogout:
+            setErrorTitle("User logged out");
+            setErrorVisible(true);
+            break;
+        }
       } else {
-        console.error(e);
-        alert(e);
+        errorStore.setError({ error: e });
       }
     }
   }
 
-  return <h1>Loading...</h1>;
+  return (
+    <>
+      <TModal
+        onPress={async () => {
+          navigation("/");
+          setErrorVisible(false);
+        }}
+        icon="star"
+        iconColor="warning"
+        title={errorTitle}
+        buttonLabel="Try again"
+        open={errorVisible}
+      >
+        <></>
+      </TModal>
+      <div className="container">
+        <div>
+          <TH2>Logging in...</TH2>
+        </div>
+        <div>
+          <TProgressCircle />
+        </div>
+      </div>
+    </>
+  );
 }
