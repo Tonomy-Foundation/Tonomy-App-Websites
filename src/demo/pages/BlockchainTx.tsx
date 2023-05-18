a1bd8cd9ad4769210724b8224f0e3ebaf1cf927c
sadiabbasi <sadiaabbas326@gmail.com>
2023-05-18 14:07:28 +0500

    update github link

--- src/demo/pages/BlockchainTx.tsx
+++ src/demo/pages/BlockchainTx.tsx
@@ -122,7 +122,7 @@ export default function BlockchainTx() {
             </button>
           `}
         documentLink="https://docs.tonomy.foundation"
-        githubLink="https://github.com/Tonomy-Foundation/Tonomy-App-Websites/blob/master/src/demo/pages/Home.tsx"
+        githubLink="https://github.com/Tonomy-Foundation/Tonomy-App-Websites/blob/development/src/demo/pages/blockchainTx.tsx"
       />
     </div>
   );
