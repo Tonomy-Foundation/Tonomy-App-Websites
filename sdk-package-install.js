32ce89b59dcc8e5ad47e424fdb09c64823b872c7
sadiabbasi <sadiaabbas326@gmail.com>
2023-06-13 00:14:26 +0500

    added isntall script

---
+++ sdk-package-install.js
@@ -0,0 +1,11 @@
+import { execSync } from "child_process";
+
+const currentBranch = execSync("git symbolic-ref --short HEAD", {
+  encoding: "utf8",
+}).trim();
+
+if (currentBranch === "feature/232-sdk-deployment") {
+  execSync("yarn add @tonomy/tonomy-id-test@development", {
+    stdio: "inherit",
+  });
+}
