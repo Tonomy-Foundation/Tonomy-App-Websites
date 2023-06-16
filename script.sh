ba75804a385bb604795575db8afba5f987f66c87
sadiabbasi <sadiaabbas326@gmail.com>
2023-06-16 18:09:59 +0500

    change js to script

--- script.sh
+++ script.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+
 echo "banch name $VERCEL_GIT_COMMIT_REF"
-if [[ $VERCEL_GIT_COMMIT_REF == "main"  ]] ; then 
-
-  echo "This is our main branch"
-
+if [[ $VERCEL_GIT_COMMIT_REF !== "master"  ]] ; then 
+  echo "execution start"
+  yarn add @tonomy/tonomy-id-sdk@development
+  echo "execution end"
 else 
-
   echo "This is not our main branch"
-
 fi
