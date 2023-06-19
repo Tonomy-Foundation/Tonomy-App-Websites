006d63e008d0dcf084cfd6e03ef115954ab35ea2
sadiabbasi <sadiaabbas326@gmail.com>
2023-06-19 14:27:27 +0500

    added exit in vercel script

--- install-tonomy-id-sdk.sh
+++ install-tonomy-id-sdk.sh
@@ -5,6 +5,8 @@ if [[ $VERCEL_GIT_COMMIT_REF != "master"  ]] ; then
   echo "execution start"
   yarn add @tonomy/tonomy-id-sdk@development
   echo "execution end"
+  exit 0
 else 
   echo "This is not our main branch"
+  exit 1
 fi
