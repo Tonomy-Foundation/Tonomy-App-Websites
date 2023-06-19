137f8a75ec5e7dfc9162f72f2e19eb631131b555
sadiabbasi <sadiaabbas326@gmail.com>
2023-06-19 14:32:48 +0500

    updated script

--- install-tonomy-id-sdk.sh
+++ install-tonomy-id-sdk.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-echo "banch name $VERCEL_GIT_COMMIT_REF"
-if [[ $VERCEL_GIT_COMMIT_REF != "master"  ]] ; then 
-  echo "execution start"
-  yarn add @tonomy/tonomy-id-sdk@development
-  echo "execution end"
-  exit 0
+echo "Branch name: $VERCEL_GIT_COMMIT_REF"
+
+if [[ $VERCEL_GIT_COMMIT_REF != "master" ]]; then 
+  echo "Execution start"
+  echo "Installing @tonomy/tonomy-id-sdk@development..."
+
+  if yarn add @tonomy/tonomy-id-sdk@development; then
+    echo "Package installation completed."
+    echo "Execution end"
+    exit 0  # Exit with a success code
+  else
+    echo "Error: Package installation failed."
+    exit 1  # Exit with an error code
+  fi
 else 
   echo "This is not our main branch"
-  exit 1
+  exit 0  # Exit with a success code
 fi
