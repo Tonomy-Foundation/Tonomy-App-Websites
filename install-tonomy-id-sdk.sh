8e6fbb25b34e7caa1eb77d1a0dddaa4f09bd1575
sadiabbasi <sadiaabbas326@gmail.com>
2023-06-19 18:06:29 +0500

    Call script in vercel.json

--- install-tonomy-id-sdk.sh
+++ install-tonomy-id-sdk.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-echo "Branch name: $VERCEL_GIT_COMMIT_REF"
-
-if [[ $VERCEL_GIT_COMMIT_REF != "master" ]]; then 
-  echo "Execution start"
-  echo "Installing @tonomy/tonomy-id-sdk@development..."
-
-  if yarn add @tonomy/tonomy-id-sdk@development; then
-    echo "Package installation completed."
-    echo "Execution end"
-    exit 0  # Exit with a success code
-  else
-    echo "Error: Package installation failed."
-    exit 1  # Exit with an error code
-  fi
+echo "banch name $VERCEL_GIT_COMMIT_REF"
+if [[ $VERCEL_GIT_COMMIT_REF != "master"  ]] ; then 
+  echo "execution start"
+  yarn add @tonomy/tonomy-id-sdk@development
+  echo "execution end"
 else 
   echo "This is not our main branch"
-  exit 0  # Exit with a success code
 fi
