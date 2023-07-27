c86e75b379a7669514011bc4d6555c29d4474d95
sadiabbasi <sadiaabbas326@gmail.com>
2023-07-28 00:51:30 +0500

    finalized task 112

--- src/demo/pages/W3CVCs.tsx
+++ src/demo/pages/W3CVCs.tsx
@@ -1,20 +1,9 @@
 import React, { useState } from "react";
 import { TH2, TP } from "../../common/atoms/THeadings";
 import { TButton } from "../../common/atoms/TButton";
-import {
-  ContainerStyle,
-  PageIntroStyle,
-  BoxContainer,
-} from "../components/styles";
-import HighlightedPageView from "../components/TPageHighlighted";
-import HorizontalLabelInput from "../components/HorizontalLabelInput";
-import TImage from "../../common/atoms/TImage";
-import Ticket from "../assets/emojis/ticket.png";
-import Handshake from "../assets/emojis/handshake.png";
-import Money from "../assets/emojis/money.png";
-import Vote from "../assets/emojis/vote.png";
-import Cruise from "../assets/emojis/cruise.png";
-import "./W3CVCs.css";
+import userLogo from "../assets/user.png";
+import VCBanner from "../assets/VC-banner.png";
+import TextboxLayout from "../components/TextboxLayout";
 import { useUserStore } from "../../common/stores/user.store";
 import { randomString, api } from "@tonomy/tonomy-id-sdk";
 import useErrorStore from "../../common/stores/errorStore";
@@ -22,6 +11,8 @@ import TModal from "../../common/molecules/TModal";
 import { VerifiableCredential } from "@tonomy/tonomy-id-sdk/build/sdk/types/sdk/util/ssi/vc";
 import { VerifiedCredential } from "@tonomy/did-jwt-vc";
 import TIcon from "../../common/atoms/TIcon";
+import CodeSnippetPreview from "../components/CodeSnippetPreview";
+import "./newW3CVCs.css";
 
 export default function W3CVCs() {
   const [name, setName] = useState("Johnathan Doe");
@@ -122,126 +113,123 @@ export default function W3CVCs() {
           )}
         </>
       </TModal>
-      <ContainerStyle>
-        <PageIntroStyle>
-          <TP className="text-header marginTop">
-            Demo feature of how to use Tonomy ID to
-          </TP>
-          <TH2> Signing Verifiable Credentials</TH2>
-          <TP className="text-header">
-            By digitally signing the medical record, you can help protect it
-            from unauthorized access and tampering, while also providing a
-            traceable record of who has accessed the information. <br /> Whether
-            you're a medical professional or someone who needs to sign and
-            verify sensitive information, the W3C verifiable credential standard
-            can help ensure trust and security in digital transactions.
-          </TP>
-          <TP>
-            Below we will illustrate the process of signing verifiable
-            credentials on our demo website.
-          </TP>
-          <TP className="list">
-            Other examples of how to use credentials with Tonomy ID include:
-            <ul>
-              <li>
-                <TImage src={Handshake} alt="ticket" className="listIcon" />
-                employment contracts
-              </li>
-              <li>
-                <TImage src={Cruise} alt="ticket" className="listIcon" />
-                shipping and logistics
-              </li>
-              <li>
-                <TImage src={Money} alt="ticket" className="listIcon" />
-                events payments and payment metadata
-              </li>
-              <li>
-                <TImage src={Vote} alt="ticket" className="listIcon" />
-                votes event
-              </li>
-              <li>
-                <TImage src={Ticket} alt="ticket" className="listIcon" />
-                tickets and much more!
-              </li>
-            </ul>
-          </TP>
-          <BoxContainer className="clientBox">
-            <TH2>Client details</TH2>
-            <div className="formView marginTop">
-              <HorizontalLabelInput
-                label="Name:"
-                value={name}
-                onChange={setName}
-              />
-              <HorizontalLabelInput
-                label="Phone number:"
-                value={phone}
-                onChange={setPhone}
-              />
-              <HorizontalLabelInput
-                label="Address:"
-                value={address}
-                onChange={setAddress}
-              />
-              <HorizontalLabelInput
-                label="Birth Date:"
-                value={dob}
-                onChange={setDob}
-              />
-              <HorizontalLabelInput
-                label="Weight:"
-                value={weight}
-                onChange={setWeight}
-              />
-              <HorizontalLabelInput
-                label="Height:"
-                value={height}
-                onChange={setHeight}
-              />
-              <HorizontalLabelInput
-                label="Allergies:"
-                value={allergies}
-                onChange={setAllergies}
-              />
-              <HorizontalLabelInput
-                label="Medication:"
-                value={medications}
-                onChange={setMedications}
-              />
-              <HorizontalLabelInput
-                label="Treatment plan:"
-                value={treatment}
-                onChange={setTreatment}
-              />
-              <div className="security-message">
-                {" "}
-                This data is fully private never stored on servers.{" "}
-                <a className="linkColor">Learn more</a>
-              </div>
-              <TButton className="btnStyle1" onClick={onSubmit}>
-                Sign medical record
-              </TButton>
+      <div className="containerVC">
+        <div className="userSectionVC">
+          <p className="leftText sign-dcoument">Feature Name: Sign Document</p>
+          <p className="userLogoVC">
+            {<img src={userLogo} alt="userLogo" />}
+            <span>Jack Tanner</span>
+          </p>
+        </div>
+        <div className="gridView">
+          <div className="leftSection">
+            <div className="headSection">
+              <p className="howText">How to use:</p>
+              <p className="tonomy-head">
+                Tonomy <span className="tonomy-d">ID</span>
+              </p>
             </div>
-          </BoxContainer>
-        </PageIntroStyle>
-        <HighlightedPageView
-          highlighterText={`
-            function onButtonPress() {
-              userApps.onPressLogin(
-              { callbackPath: "/callback" },
-              new JsKeyManager()
-              );
-              ...
-            }
-            <button className="tonomy-login-button"
-            onClick={onButtonPress}>
-            Login with {Your Platform Name Here}
-            </button>
-          `}
-          documentLink="https://docs.tonomy.foundation"
-          githubLink="https://github.com/Tonomy-Foundation/Tonomy-App-Websites/blob/master/src/demo/pages/Home.tsx"
+            <p className="paraDetail">
+              Sign and verify sensitive information with Tonomy ID. The W3C
+              Verifiable Credential standard can help ensure trust and security
+              when sharing sensitive and tamper-proof data.
+            </p>
+            <a href="#" className="paraLink">
+              Learn about the W3C Verifiable Credentials {`->`}
+            </a>
+            <p className="demoLink">Enter Demo</p>
+          </div>
+          <div className="rightSection">
+            <img src={VCBanner} alt="banner-image" className="bannerImg" />
+          </div>
+        </div>
+      </div>
+      <div className="paraSection">
+        <p className="imagine">Imagine,</p>
+        <p className="paralines">
+          {`you go to the doctor's office for a checkup. While waiting, your
+          Tonomy ID notifies you that Dr. Smith wants access to your medical
+          files. With just one click, you can grant access to the files while
+          waiting for the doctor to arrive.`}
+        </p>
+      </div>
+      <div className="formSection">
+        <ul className="horizontal-list">
+          <li>Appointment</li>
+          <li>Messages</li>
+          <li className="border-bottom-margin">
+            <span></span>Results
+          </li>
+        </ul>
+        <div className="clientSection">
+          <h4 className="head">Client details</h4>
+
+          <TextboxLayout label="Name:" value={name} onChange={setName} />
+          <TextboxLayout
+            label="Phone number:"
+            value={phone}
+            onChange={setPhone}
+          />
+          <TextboxLayout
+            label="Address:"
+            value={address}
+            onChange={setAddress}
+          />
+          <TextboxLayout label="Birth Date:" value={dob} onChange={setDob} />
+          <div className="row-container">
+            <TextboxLayout
+              label="Weight:"
+              value={weight}
+              onChange={setWeight}
+            />
+            <TextboxLayout
+              label="Height:"
+              value={height}
+              onChange={setHeight}
+            />
+          </div>
+          <TextboxLayout
+            label="Allergies:"
+            value={allergies}
+            onChange={setAllergies}
+          />
+          <TextboxLayout
+            label="Medication:"
+            value={medications}
+            onChange={setMedications}
+          />
+          <TextboxLayout
+            label="Treatment plan:"
+            value={treatment}
+            onChange={setTreatment}
+          />
+          <div className="security-message">
+            {" "}
+            This data is fully private never stored on servers.{" "}
+            <a className="linkColor">Learn more</a>
+          </div>
+          <div>
+            <TButton className="btnStyle1" onClick={onSubmit}>
+              Sign using your tonomy DID
+            </TButton>
+          </div>
+        </div>
+        <CodeSnippetPreview
+          value={`
+  function onButtonPress() {
+  userApps.onPressLogin(
+    { callbackPath: "/callback" },
+    new JsKeyManager()
+  );
+  ...
+  }
+  <button className="tonomy-login-button"
+  onClick={onButtonPress}>
+  Login with {Your Platform Name Here}
+  </button>
+            `}
         />
-      </ContainerStyle>
+      </div>
     </>
   );
 }
--- src/demo/routes/authRoutes.tsx
+++ src/demo/routes/authRoutes.tsx
@@ -1,21 +1,11 @@
 import { RouteObject } from "react-router-dom";
 import Home from "../pages/Home";
 import Callback from "../pages/Callback";
-import W3CVCs from "../pages/newW3CVCs";
-import UserHome from "../pages/UserHome";
 
 const authRoutes: RouteObject[] = [
   {
     path: "/",
-    element: <W3CVCs />,
-  },
-  {
-    path: "/w3c-vcs",
-    element: <W3CVCs />,
-  },
-  {
-    path: "/user-home",
-    element: <UserHome />,
+    element: <Home />,
   },
   {
     path: "/callback",
