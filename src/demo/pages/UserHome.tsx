cf6a9de6c18821bd3ba0ae88d06f11241b1fc548
sadiabbasi <sadiaabbas326@gmail.com>
2023-07-31 03:29:03 +0500

    fix the username in home page

--- src/demo/pages/UserHome.tsx
+++ src/demo/pages/UserHome.tsx
@@ -1,42 +1,28 @@
 import React, { useEffect, useState } from "react";
 import ImageSlider from "../components/ImageSlider";
-import user from "../assets/user.png";
+import userLogo from "../assets/user.png";
 import { ContainerStyle } from "../components/styles";
 import { useUserStore } from "../../common/stores/user.store";
 import { api } from "@tonomy/tonomy-id-sdk";
 import useErrorStore from "../../common/stores/errorStore";
-import settings from "../../common/settings";
 import { images, linkTexts } from "./userHomeHelper";
 import "./UserHome.css";
 
 const USerHome: React.FC = () => {
-  const userStore = useUserStore();
   const errorStore = useErrorStore();
   const [username, setUsername] = useState<string>("");
-  const [blockExplorerUrl, setBlockExplorerUrl] = useState<string>("");
+  let user = useUserStore((state) => state.user);
 
   async function onRender() {
     try {
-      if (!userStore.user) {
-        const user = await api.ExternalUser.getUser();
-
-        userStore.setUser(user);
-
-        const username = await user.getUsername();
-
-        if (!username) throw new Error("No username found");
-        setUsername(username.getBaseUsername());
-
-        const accountName = await user?.getAccountName();
-
-        if (!accountName) throw new Error("No account name found");
-        let url = "https://local.bloks.io/account/" + accountName + "?nodeUrl=";
-
-        url += settings.isProduction()
-          ? settings.config.blockchainUrl
-          : "http://localhost:8888";
-        setBlockExplorerUrl(url);
+      if (!user) {
+        user = await api.ExternalUser.getUser();
       }
+
+      const username = await user.getUsername();
+
+      if (!username) throw new Error("No username found");
+      setUsername(username.getBaseUsername());
     } catch (e) {
       errorStore.setError({ error: e, expected: false });
     }
@@ -49,7 +35,7 @@ const USerHome: React.FC = () => {
   return (
     <ContainerStyle>
       <div className="userSection">
-        <img src={user} alt="userLogo" className="userLogo" />
+        <img src={userLogo} alt="userLogo" className="userLogo" />
         <span>{username}</span>
       </div>
       <div>
