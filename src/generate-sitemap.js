ca2519376a3eceb4737d7c673f5d755de7433829
sadiabbasi <sadiaabbas326@gmail.com>
2023-08-16 18:05:39 +0500

    read url from sitemap

--- src/generate-sitemap.js
+++ src/generate-sitemap.js
@@ -56,8 +56,7 @@ const accountsRoutes = [
   },
 ];
 
-const sitemapXml = `
-  <?xml version="1.0" encoding="UTF-8"?>
+const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${demoRoutes
       .map(
