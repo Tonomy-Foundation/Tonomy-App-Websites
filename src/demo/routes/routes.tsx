22ad26c2d245f9ddbd06eea905915e846b77fc37
sadia <sadia@Ubuntu20-dev.myguest.virtualbox.org>
2023-11-01 18:36:52 +0300

    worked on home screen responsive

--- src/demo/routes/routes.tsx
+++ src/demo/routes/routes.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PrivateRoute from "./guards/PrivateRoute";
 import HomeScreen from "../pages/Home";
+import MobileHomeScreen from "../mobilePages/Home";
 import Callback from "../pages/Callback";
 import MainLayout from "../layout/MainLayout";
 import mainRoutes from "./mainRoutes";
 import AuthProvider from "../providers/AuthProvider";
 
 export default function RootRoutes(): JSX.Element {
+  const isMobile = window.innerWidth <= 768;
   return (
     <Router>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<HomeScreen />} />
+          <Route path="/" element={isMobile? <MobileHomeScreen /> :<HomeScreen />} />
           <Route path="/callback" element={<Callback />} />
           {mainRoutes.map((route) => (
             <Route
