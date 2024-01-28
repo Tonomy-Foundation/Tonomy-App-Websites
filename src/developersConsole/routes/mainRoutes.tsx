3e4cb66dc0d8f4b0885211d4970db66ddd32d476
sadiabbasi <sadiaabbas326@gmail.com>
2024-01-28 17:17:24 +0300

    finalized the UI

--- src/developersConsole/routes/mainRoutes.tsx
+++ src/developersConsole/routes/mainRoutes.tsx
@@ -1,5 +1,11 @@
 import { RouteObject } from "react-router-dom";
+import UserHome from "../pages/UserHome";
 
-const mainRoutes: RouteObject[] = [];
+const mainRoutes: RouteObject[] = [
+  {
+    path: "/user-home",
+    element: <UserHome />,
+  },
+];
 
 export default mainRoutes;
--- src/developersConsole/routes/routes.tsx
+++ src/developersConsole/routes/routes.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AuthProvider from "../providers/AuthProvider";
+import mainRoutes from "./mainRoutes";
+import PrivateRoute from "./guards/PrivateRoute";
 import HomeScreen from "../pages/Home";
 import Callback from "../pages/Callback";
-import AuthProvider from "../providers/AuthProvider";
 
 export default function RootRoutes(): JSX.Element {
   return (
@@ -11,6 +13,14 @@ export default function RootRoutes(): JSX.Element {
         <Routes>
           <Route path="/" element={<HomeScreen />} />
           <Route path="/callback" element={<Callback />} />
+
+          {mainRoutes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={<PrivateRoute>{route.element}</PrivateRoute>}
+            />
+          ))}
         </Routes>
       </AuthProvider>
     </Router>
