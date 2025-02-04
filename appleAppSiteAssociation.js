c926447172eb761c3d71cec6177b640d5ed07203
sadiabbasi <sadiaabbas326@gmail.com>
2025-02-04 17:46:19 +0300

    fix: :zap: update component route

--- appleAppSiteAssociation.js
+++ appleAppSiteAssociation.js
@@ -33,7 +33,7 @@ const appleAppSiteAssociation = {
         appIDs: [],
         components: [
           {
-            "/": "/login",
+            "/": "/login/*",
             "?": {
               payload: "*",
             },
