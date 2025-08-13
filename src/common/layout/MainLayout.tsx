ff40e6a025cbe15ea9e6c0d35ceac7c9e8cb0315
sadiabbasi <sadiaabbas326@gmail.com>
2025-08-13 18:45:32 +0300

    fix: :zap: fixed build error

--- src/common/layout/MainLayout.tsx
+++ src/common/layout/MainLayout.tsx
@@ -1,7 +1,12 @@
 // AppLayout.jsx
 import TopMenuBar from "./TopMenuBar";
 
-export default function AppLayout({ children, page = undefined }) {
+type AppLayoutProps = {
+  children: React.ReactNode;
+  page?: string;
+};
+
+export default function AppLayout({ children, page }: AppLayoutProps) {
   return (
     <div className="flex flex-col min-h-screen">
       <TopMenuBar page={page} />
